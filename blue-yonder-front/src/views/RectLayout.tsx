19da8fe0073b8b60c3fbe4acdce22cee3bd54c71
MarioJim <mario.emilio.j@gmail.com>
2020-08-29 18:53:33 -0500

    Added sketch for RectLayout

--- blue-yonder-front/src/views/RectLayout.tsx
+++ blue-yonder-front/src/views/RectLayout.tsx
@@ -1,57 +1,35 @@
 import React, { useState } from 'react';
-import StyledNumberInput from '../components/StyledNumberInput';
+import { calcRectPacking, DrawingParams } from '../algorithms/rectPacking';
+import RectLayoutInputs from '../components/RectLayoutInputs';
+import RectLayoutSketch from '../components/RectLayoutSketch';
+import styled from '@emotion/styled';
 
-interface DrawingParams {
-  widthPlace: number;
-  heightPlace: number;
-  widthTable: number;
-  heightTable: number;
-}
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 30% 70%;
+`;
 
 const RectLayout: React.FC = () => {
   const [drawingParams, setDrawingParams] = useState<DrawingParams>({
-    widthPlace: 0,
-    heightPlace: 0,
-    widthTable: 0,
-    heightTable: 0,
+    widthPlace: 20,
+    heightPlace: 10,
+    widthTable: 1,
+    heightTable: 0.7,
+    distanceBetween: 1.5,
   });
-  const handleChange = (prop: keyof DrawingParams) => (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setDrawingParams({ ...drawingParams, [prop]: +event.target.value });
-  };
   return (
     <div>
       <h1>RectLayout</h1>
-      <form>
-        <StyledNumberInput
-          title="Ancho del lugar"
-          unit="m"
-          value={drawingParams.widthPlace}
-          onChange={handleChange('widthPlace')}
+      <Wrapper>
+        <RectLayoutInputs
+          drawingParams={drawingParams}
+          setDrawingParams={setDrawingParams}
         />
-        <br />
-        <StyledNumberInput
-          title="Largo del lugar"
-          unit="m"
-          value={drawingParams.heightPlace}
-          onChange={handleChange('heightPlace')}
+        <RectLayoutSketch
+          drawingParams={drawingParams}
+          rects={calcRectPacking(drawingParams)}
         />
-        <br />
-        <StyledNumberInput
-          title="Ancho de una mesa"
-          unit="m"
-          value={drawingParams.widthTable}
-          onChange={handleChange('widthTable')}
-        />
-        <br />
-        <StyledNumberInput
-          title="Largo de una mesa"
-          unit="m"
-          value={drawingParams.heightTable}
-          onChange={handleChange('heightTable')}
-        />
-      </form>
+      </Wrapper>
     </div>
   );
 };
