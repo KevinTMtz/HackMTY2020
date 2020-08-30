6549c5599e70adae1644fa476b738c3a4fb67c73
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-08-29 21:02:08 -0500

    Login & Register panel centered alignment.

--- blue-yonder-front/src/views/RegisterPage.tsx
+++ blue-yonder-front/src/views/RegisterPage.tsx
@@ -6,6 +6,11 @@ import { auth, db } from '../firebase';
 const StyledTextField = styled.div`
   display: flex;
   justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 
   h2 {
     margin: 0px 0px 20px 0px;
