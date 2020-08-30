d083790b3bca2d789d027d79285283fe30b5ba01
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-08-29 19:46:44 -0500

    Firebase authentication.

---
+++ blue-yonder-front/src/firebase/firebase.ts
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+  apiKey: 'AIzaSyCX2VcoHnj0E7uBt92jxE4wqGMg5Ujzea0',
+  authDomain: 'space-planner-d632f.firebaseapp.com',
+  databaseURL: 'https://space-planner-d632f.firebaseio.com',
+  projectId: 'space-planner-d632f',
+  storageBucket: 'space-planner-d632f.appspot.com',
+  messagingSenderId: '767052313900',
+  appId: '1:767052313900:web:302162203ebaf0fd2a6889',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export const auth = firebase.auth();
+export const db = firebase.database();
